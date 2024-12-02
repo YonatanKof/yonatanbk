33f2273cdcb293a9959a68bdc5c894aad45a3e6b
Yonatan Kof <kof@yonatankof.com>
2024-12-02 15:25:23 +0200

    feat: Add Bricolage Grotesque font and preference

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	// modules: [],
+	app: {
+		head: {
+			link: [
+				{
+					rel: 'preload',
+					as: 'font',
+					href: '/fonts//fonts/BricolageGrotesque[opsz,wdth,wght].woff2',
+					type: 'font/woff2',
+					crossorigin: 'anonymous',
+				},
+			],
+		},
+	},
 	css: ['~/assets/styles/index.css'],
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
