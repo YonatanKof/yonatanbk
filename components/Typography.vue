5d6aa2e79518106c188f727114d4bc720c792bf5
Yonatan Kof <kof@yonatankof.com>
2024-12-03 10:03:11 +0200

    feat: Add Typography comp

---
+++ components/Typography.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+type Variant = 'large' | 'small' | 'text';
+
+const { tag = 'p', variant = 'text' } = defineProps<{
+	tag?: 'h1' | 'h2' | 'p'; // to h6
+	variant?: Variant;
+}>();
+
+const VARIANT_CLASSES: Record<Variant, string> = {
+	large: 'heading-large', // font-bold text-5xl
+	small: 'heading-small',
+	text: 'text',
+};
+
+const variantClasses = computed(() => {
+	return VARIANT_CLASSES[variant];
+});
+</script>
+
+<template>
+	<component :is="tag" :class="variantClasses">
+		<slot />
+	</component>
+</template>
+
+<style scoped>
+.heading-large {
+	text-transform: capitalize;
+	font-variation-settings: 'wght' 800, 'wdth' 100, 'opsz' 96;
+	font-size: var(--step-8);
+	text-align: center;
+  line-height: 1.1;
+}
+.heading-small {
+	font-size: 30pt;
+	text-decoration: underline;
+}
+.text {
+	text-decoration: underline;
+}
+</style>
