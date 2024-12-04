3e0c008d5e578048a70a8c0f196d99413f425261
Yonatan Kof <kof@yonatankof.com>
2024-12-04 23:37:28 +0200

    improve: Add styles, images and content in YonatanKof.vue section

--- components/Section/YonatanKof.vue
+++ components/Section/YonatanKof.vue
@@ -1,22 +1,59 @@
 <script setup lang="ts"></script>
 
 <template>
-	<section id="yonatan-kof" class="checked-grid-lines section">
+	<section id="yonatan-kof" class="section">
 		<div class="section-content">
-			<Text tag="h1" variant="large"
-				>A full stack Designer <br />
-				Who Leads Design Initiatives
-			</Text>
+			<div id="main-content">
+				<span>
+					<div id="avatar">
+						<NuxtImg src="/images/kof-pic.webp" placeholder />
+						<Text tag="h2" variant="small">Hi, I’m Yonatan Kof </Text>
+					</div>
+					<Text class="title" tag="h1" variant="large">A full stack Designer </Text>
+				</span>
+				<span>
+					<Text class="text-end" tag="h1" variant="large"> Who Leads Design Initiatives </Text>
+					<Text id="main-message" class="text-end" tag="h2" variant="small">Perfect to Manage design Teams in Startups and Orgs</Text>
+				</span>
+			</div>
 		</div>
-	</section>
-	<section id="design-connection" class="section">
-		<div class="section-content">
-			<Text tag="h1" variant="large">Design Connection</Text>
+		<div id="design-connection" class="section">
+			<div class="section-content">
+				<Text tag="h1" variant="large">Design Connection</Text>
+			</div>
 		</div>
 	</section>
 </template>
 
 <style scoped>
+.text-end {
+	text-align: end;
+}
+#main-content {
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+}
+#avatar,
+#main-message {
+	color: var(--color-black-slight);
+}
+#main-message {
+	justify-content: end;
+	margin-block-start: var(--space-s);
+	text-align: end;
+}
+#avatar {
+	display: flex;
+	align-items: center;
+	gap: var(--space-s);
+	margin-inline-start: calc(calc(var(--step-6) + var(--space-s)) * -1);
+}
+#avatar img {
+	border-radius: 100%;
+	width: var(--step-6);
+	height: var(--step-6);
+}
 #design-connection {
 	background-color: var(--color-brand-red-main);
 }
