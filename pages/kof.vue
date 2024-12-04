05d6eb6dec9287c013bf2ba06cdb65a972b42793
Yonatan Kof <kof@yonatankof.com>
2024-12-04 23:41:04 +0200

    feat: Add test page

---
+++ pages/kof.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="block-end">
+		<Text class="text-end" tag="h1" variant="large"> Who Leads Design Initiatives </Text>
+		<Text class="text-end" tag="h2" variant="small">Perfect to Manage design Teams in Startups and Orgs</Text>
+	</div>
+</template>
+
+<style scoped>
+#block-end {
+	display: flex;
+	flex-direction: column;
+}
+.text-end {
+	text-align: end;
+}
+</style>
