d4fe9e2d8728c4f44e0169b8197d48198ffcc036
Yonatan Kof <kof@yonatankof.com>
2024-12-06 10:01:29 +0200

    improve: Yonatan kof section design

--- components/Section/YonatanKof.vue
+++ components/Section/YonatanKof.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-	<section id="yonatan-kof" class="section">
-		<div class="section-content">
-			<div id="main-content">
+	<section id="yonatan-kof">
+		<div id="main-messages" class="section-block">
+			<div class="padding-center-center">
 				<span>
 					<div id="avatar">
 						<NuxtImg src="/images/kof-pic.webp" placeholder />
@@ -13,12 +13,14 @@
 				</span>
 				<span>
 					<Text class="text-end" tag="h1" variant="large"> Who Leads Design Initiatives </Text>
-					<Text id="main-message" class="text-end" tag="h2" variant="small">Perfect to Manage design Teams in Startups and Orgs</Text>
+					<Text id="main-message" class="text-end" tag="h2" variant="small">
+						Perfect to Manage design Teams in Startups and Orgs
+					</Text>
 				</span>
 			</div>
 		</div>
-		<div id="design-connection" class="section">
-			<div class="section-content">
+		<div id="secondary-messages" class="section-block">
+			<div class="padding-center-center">
 				<Text tag="h1" variant="large">Design Connection</Text>
 			</div>
 		</div>
@@ -26,38 +28,55 @@
 </template>
 
 <style scoped>
+.title {
+	width: fit-content;
+	position: relative;
+}
+.title::before {
+	content: '';
+	background-image: url('/images/arrow-v1.webp');
+	display: inline-block;
+	width: var(--step-3);
+	/* width: var(--space-m-l); */
+	aspect-ratio: 136/214;
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+	position: absolute;
+	transform: rotate(115deg);
+	inset-block-end: var(--step-3);
+	inset-inline-start: calc(var(--step-3) * -1);
+}
+/* span:first-child {
+	color: aquamarine;
+} */
 .text-end {
 	text-align: end;
 }
-#main-content {
-	display: flex;
-	align-items: center;
-	flex-direction: column;
+#main-messages {
+
 }
 #avatar,
-#main-message {
+#main-messages {
 	color: var(--color-black-slight);
 }
-#main-message {
-	justify-content: end;
-	margin-block-start: var(--space-s);
-	text-align: end;
-}
+
 #avatar {
 	display: flex;
 	align-items: center;
 	gap: var(--space-s);
-	margin-inline-start: calc(calc(var(--step-6) + var(--space-s)) * -1);
+	margin-inline-start: calc(calc(var(--step-6) + var(--space-3xs)) * -1);
 }
 #avatar img {
 	border-radius: 100%;
 	width: var(--step-6);
 	height: var(--step-6);
 }
-#design-connection {
+#secondary-messages {
 	background-color: var(--color-brand-red-main);
+	height: 100%;
 }
-#design-connection h1 {
+#secondary-messages h1 {
 	color: var(--color-sys-invert-main);
 }
 </style>
