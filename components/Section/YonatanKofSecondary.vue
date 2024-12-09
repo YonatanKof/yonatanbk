952d490cea958b7157d085765099184226ab5c4b
Yonatan Kof <kof@yonatankof.com>
2024-12-09 11:10:54 +0200

    feat: Brake YonatanBenKnaan.vue to Primary and Secondary

---
+++ components/Section/YonatanKofSecondary.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div id="secondary-messages" class="section-block">
+		<div class="padding-center-center">
+			<Text tag="h1" variant="main-title">Design Connection</Text>
+			<Text
+				>For the last 15+ years my mission is to bridge the gap between users, businesses, and technology through
+				research and design, creating elegant solutions that simplify complexity and enhance lives.</Text
+			>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+#secondary-messages {
+	background-color: var(--color-brand-red-main);
+	height: 100%;
+}
+#secondary-messages h1 {
+	color: var(--color-sys-invert-main);
+}
+</style>
