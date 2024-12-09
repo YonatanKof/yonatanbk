80d8d994813dfe3456b7717ecac0a6b67ed6c2ea
Yonatan Kof <kof@yonatankof.com>
2024-12-09 12:02:51 +0200

    improve: Add large-text to Text.vue

--- components/Text.vue
+++ components/Text.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 // Define the variant type as a const array first
-const VARIANTS = ['main-title', 'compressed-title', 'text'] as const;
+const VARIANTS = ['main-title', 'compressed-title', 'text', 'large-text'] as const;
 // Create the type from the array values
 type Variant = (typeof VARIANTS)[number];
 
 const props = withDefaults(
 	defineProps<{
 		tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
-		variant?: 'main-title' | 'compressed-title' | 'text';
+		variant?: 'main-title' | 'compressed-title' | 'text' | 'large-text';
 	}>(),
 	{
 		tag: 'p',
@@ -19,6 +19,7 @@ const props = withDefaults(
 const VARIANT_CLASSES: Record<Variant, string> = {
 	'main-title': 'heading-large', // font-bold text-5xl
 	'compressed-title': 'heading-small',
+	'large-text': 'large-text',
 	text: 'text',
 };
 
@@ -36,14 +37,18 @@ const variantClasses = computed(() => {
 <style scoped>
 .heading-large {
 	font-variation-settings: 'wght' 800, 'wdth' 100, 'opsz' 96;
-	font-size: var(--step-7);
-	text-align: center;
-	line-height: 1.1;
+	font-size: var(--step-8);
+	line-height: 1;
 }
 .heading-small {
 	font-variation-settings: 'wght' 400, 'wdth' 86, 'opsz' 96;
 	font-size: var(--step-4);
-	line-height: 1.1;
+	line-height: 1;
+}
+.large-text {
+	font-variation-settings: var(--base-font-settings);
+	font-size: var(--step-2);
+	font-family: var(--font-body);
 }
 .text {
 	font-variation-settings: var(--base-font-settings);
