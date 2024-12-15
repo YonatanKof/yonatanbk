fcd6cd18a7ac80ed954cdb0070ee8610f6bf8c47
Yonatan Kof <kof@yonatankof.com>
2024-12-15 18:58:22 +0200

    content: Add Pepperi content to Work.vue

--- components/Section/Work.vue
+++ components/Section/Work.vue
@@ -7,6 +7,49 @@
 			corporate man - I've solved all kinds of design problems and have met with all kind of 
 			projects. Here are but a few."
 			/>
+			<div class="work">
+				<Project
+					:image-array="[
+						'/images/projects/pepperi/pepperi-personas.webm',
+						'/images/projects/pepperi/pepperi-page-builder.webp',
+						'/images/projects/pepperi/pepperi-design-system.webp',
+						'/images/projects/pepperi/pepperi-page-builder-vid.webm',
+					]"
+					title="Pepperi"
+					position="Head of Design"
+					year="2018-2024"
+					description="Lead webapp across various roles, designing new products, managing requirements, backlogs and design system, conducting research, while supporting marketing, brand and professional services team."
+					:verticals="['B2B', 'E-commerce', 'Storefront', 'No Code Editor', 'Webpage Builder']"
+					:jobs="[
+						'Research',
+						'Competitive Analysis',
+						'Stakeholder Mgmt.',
+						'Backlog Mgmt.',
+						'Design',
+						'UX',
+						'User Testing',
+					]"
+					logo="/images/clients-color/Pepperi.webp"
+				/>
+				<Project
+					order="flip"
+					title="Pepperi"
+					position="Head of Design"
+					year="2018-2024"
+					description="Lead webapp across various roles, designing new products, managing requirements, backlogs and design system, conducting research, while supporting marketing, brand and professional services team."
+					:verticals="['B2B', 'E-commerce', 'Storefront', 'No Code Editor', 'Webpage Builder']"
+					:jobs="[
+						'Research',
+						'Competitive Analysis',
+						'Stakeholder Mgmt.',
+						'Backlog Mgmt.',
+						'Design',
+						'UX',
+						'User Testing',
+					]"
+					logo="/images/clients-color/Pepperi.webp"
+				/>
+			</div>
 		</div>
 	</section>
 </template>
---
+++ components/VideoTag.vue
@@ -0,0 +1,85 @@
+<script setup>
+defineProps({
+	src: {
+		type: String,
+		default: undefined,
+	},
+	srcMp4: {
+		type: String,
+		default: undefined,
+	},
+	aspectRatio: {
+		type: String,
+		default: '1/1',
+	},
+	controls: {
+		type: Boolean,
+		default: false,
+	},
+	autoplay: {
+		type: Boolean,
+		default: true,
+	},
+	muted: {
+		type: Boolean,
+		default: true,
+	},
+	loop: {
+		type: Boolean,
+		default: true,
+	},
+	showMesh: {
+		type: Boolean,
+		default: false,
+	},
+	desc: {
+		type: String,
+		default: '',
+	},
+	marginBlock: {
+		type: String,
+		default: 'var(--space-xs)',
+	},
+	minWidth: {
+		type: String,
+		default: undefined,
+	},
+});
+</script>
+<template>
+	<div>
+		<span v-if="showMesh"></span>
+		<video :controls="controls" :autoplay="autoplay" :muted="muted" :loop="loop" playsinline>
+			<source :src="src" type="video/webm" />
+			<source :src="srcMp4" type="video/mp4" />
+			<p>Your browser does not support <code>video</code> tag</p>
+		</video>
+		<em v-if="desc">{{ desc }}</em>
+	
+	</div>
+</template>
+<style scoped>
+div {
+	margin-block: v-bind(marginBlock);
+	display: block;
+	width: 100%;
+	height: min-content;
+	position: relative;
+}
+span {
+	border-radius: var(--border-radius-xs);
+	position: absolute;
+	clear: both;
+	background-image: radial-gradient(transparent 1px, var(--color-brand-red-slight) 1px);
+	background-size: 2px 2px;
+	inset: 0;
+}
+video {
+	max-width: 100%;
+	aspect-ratio: v-bind(aspectRatio);
+	border-radius: var(--border-radius-xs);
+	overflow: hidden;
+	display: flow-root;
+	min-width: v-bind(minWidth);
+}
+</style>
