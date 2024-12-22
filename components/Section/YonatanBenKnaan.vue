be48ca30dd7c95033410ee1d2730cce0f83770c9
Yonatan Kof <kof@yonatankof.com>
2024-12-22 16:08:19 +0200

    improve: layout for medium small screens in YonatanBenKnaan.vue

--- components/Section/YonatanBenKnaan.vue
+++ components/Section/YonatanBenKnaan.vue
@@ -21,6 +21,9 @@ const { open, close } = useModal({
 					<Text tag="h2" variant="compressed-title" text-style="color-slight">Yonatan Ben Knaan, </Text>
 				</div>
 				<Text id="main-title-1" tag="h1" variant="main-title" text-style="grid-size-big">a full stack designer </Text>
+				<Text id="main-title-single" tag="h1" variant="main-title" text-style="grid-size-big"
+					>a full stack design lead
+				</Text>
 			</div>
 
 			<div id="main-messages-bottom">
@@ -28,14 +31,14 @@ const { open, close } = useModal({
 					>who leads <span class="no-brake">design initiatives</span>
 				</Text>
 				<Text id="flipping-message" text-style="text-end color-slight" tag="h2" variant="compressed-title">
-					that Manage design Teams in Startups and Orgs
+					that Manage design Teams in <span>Startups and Orgs</span>
 				</Text>
 			</div>
 			<span id="logos-cta">
 				<ClientProof />
 				<div class="cta-buttons">
 					<Button
-						style="padding: unset"
+						style="padding-inline: unset"
 						button-style="secondary"
 						tag="a"
 						href="https://www.linkedin.com/in/yonatankof/"
@@ -78,37 +81,44 @@ const { open, close } = useModal({
 	grid-template-columns: repeat(var(--grid-num), var(--grid-block));
 	justify-content: center;
 	/* transition: max-width 0.25s ease-in-out, grid-template-columns 0.25s ease-in-out; */
-	/* @media (width <= 1408px) {
+	/* @media (width < 1408px) {
 		--grid-num: 37;
 	} */
-	/* @media (width <= 1280px) {
+	/* @media (width < 1280px) {
 		--grid-num: 37;
 	} */
-	@media (width <= 1216px) {
+	@media (width < 1216px) {
 		--grid-num: 37;
 	}
-	@media (width <= 1152px) {
+	@media (width < 1152px) {
 		--grid-num: 35;
 	}
-	@media (width <= 960px) {
+	@media (width < 960px) {
 		--grid-num: 33;
 	}
-	@media (width <= 896px) {
+	@media (width < 896px) {
 		--grid-num: 31;
 	}
-	/* @media (width <= 768px) {
-		--grid-num: 27;
-	} */
-}
-.cta-buttons {
-	display: flex;
-	flex-direction: row;
-	gap: calc(var(--grid-block) / 2);
-	& button {
-		width: calc(var(--grid-block) * 6);
+	@media (width < 704px) {
+		--grid-num: 23;
 	}
-	& a {
-		width: calc(var(--grid-block) * 8);
+	@media (width < 640px) {
+		--grid-num: 21;
+	}
+	@media (width < 600px) {
+		--grid-num: 19;
+	}
+	@media (width < 560px) {
+		--grid-num: 17;
+	}
+	@media (width < 520px) {
+		--grid-num: 15;
+	}
+	@media (width < 480px) {
+		--grid-num: 13;
+	}
+	@media (width < 400px) {
+		--grid-num: 11;
 	}
 }
 
@@ -118,32 +128,33 @@ const { open, close } = useModal({
 	display: grid;
 	grid-template-rows: subgrid;
 	grid-template-columns: subgrid;
-	@media (width <= 1216px) {
+	@media (width < 1216px) {
 		grid-column: 3 / -1;
 	}
-	@media (width <= 960px) {
-		grid-column: 2 / -1;
-	}
-	@media (width <= 768px) {
+	@media (width < 960px) {
 		grid-column: 1 / -1;
 	}
-	/* @media (width <= 704px) {
-
-	} */
+	@media (width < 768px) {
+		grid-column: 1 / -1;
+	}
+	@media (width < 704px) {
+		grid-row: 1 / span 8;
+		grid-column: 1 / -1;
+	}
 }
 #main-messages-bottom {
 	grid-row: 6 / span 5;
 	grid-column: 1 / span 3;
 	display: grid;
 	grid-template-rows: subgrid;
-	justify-self: center;
+	/* justify-self: center; */
 	grid-column: 1 / -1;
-	@media (width <= 1216px) {
+	@media (width < 1216px) {
 		grid-row: 6 / span 8;
 	}
-	/* @media (width <= 768px) {
-		grid-row: 6 / span 5;
-	} */
+	@media (width < 704px) {
+		grid-row: 9 / span 3;
+	}
 }
 #avatar {
 	grid-row: 1 / span 2;
@@ -156,29 +167,35 @@ const { open, close } = useModal({
 		width: calc(var(--grid-block) * 2);
 		height: calc(var(--grid-block) * 2);
 	}
+	@media (width < 704px) {
+		grid-column: 2 / -1;
+	}
+	@media (width < 640px) {
+		grid-column: 1 / -1;
+	}
 }
 #main-title-1 {
 	grid-row: 3 / span 3;
 	grid-column: 3 / -6;
 	position: relative;
-	@media (width <= 1216px) {
+	@media (width < 1216px) {
 		grid-column: 4 / -5;
 	}
-	@media (width <= 1152px) {
+	@media (width < 1152px) {
 		grid-column: 3 / -4;
 	}
-	@media (width <= 960px) {
+	@media (width < 960px) {
+		grid-column: 3 / -3;
+	}
+	@media (width < 896px) {
 		grid-column: 3 / -2;
 	}
-	@media (width <= 896px) {
-		grid-column: 2 / -2;
-	}
-	@media (width <= 768px) {
+	@media (width < 768px) {
 		grid-column: 3 / -2;
 	}
-	/* @media (width <= 704px) {
-		grid-column: 3 / -6;
-	} */
+	@media (width < 704px) {
+		display: none;
+	}
 	&::before {
 		content: '';
 		background-image: url('/images/arrow-v1.webp');
@@ -190,22 +207,32 @@ const { open, close } = useModal({
 		background-repeat: no-repeat;
 		position: absolute;
 		transform: rotate(115deg);
-		/* inset-block-end: var(--grid-block); */
 		inset-inline-start: calc(var(--grid-block) * -1);
 	}
 }
 #main-title-2 {
 	grid-row: 1 / span 3;
-	@media (width <= 1152px) {
+	@media (width < 1152px) {
 		grid-row: 1 / span 6;
+	}
+	@media (width < 704px) {
+		display: none;
 	}
 }
 #flipping-message {
 	grid-row: 4 / span 2;
 	align-self: center;
-	@media (width <= 1152px) {
+	@media (width < 1152px) {
 		grid-row: 7 / span 2;
 		text-align: center;
+	}
+	@media (width < 704px) {
+		grid-row: 1 / -1;
+		max-width: 560px;
+		justify-self: center;
+		& span {
+			text-wrap: nowrap;
+		}
 	}
 }
 #logos-cta {
@@ -213,39 +240,135 @@ const { open, close } = useModal({
 	width: 100%;
 	display: flex;
 	justify-content: space-between;
-	gap: var(--space-xl);
+	/* gap: var(--space-xl); */
 	grid-column: 1 / -1;
-	@media (width <= 1152px) {
+	@media (width < 1152px) {
 		grid-row: 16 / span 3;
+	}
+	@media (width < 704px) {
+	
+		grid-row: 13 / span 7;
+		flex-direction: column-reverse;
+	}
+	@media (width < 480px) {
+		grid-row: 13 / span 10;
+
 	}
 }
 .client-section {
 	max-width: calc(var(--grid-block) * 20);
-	@media (width <= 1408px) {
+	@media (width < 1408px) {
 	}
-	@media (width <= 1280px) {
-		
+	@media (width < 1280px) {
 	}
-	@media (width <= 1216px) {
+	@media (width < 1216px) {
 		max-width: calc(var(--grid-block) * 18);
-		
 	}
-	@media (width <= 1152px) {
+	@media (width < 1152px) {
 		max-width: calc(var(--grid-block) * 16);
-		
 	}
-	@media (width <= 960px) {
+	@media (width < 960px) {
 		max-width: calc(var(--grid-block) * 14);
-		
 	}
-	@media (width <= 896px) {
+	@media (width < 896px) {
 		max-width: calc(var(--grid-block) * 12);
-		/* max-width: calc(var(--grid-block) * 10); */
-		
 	}
-	@media (width <= 768px) {
-		
+	@media (width < 704px) {
+		max-width: 100%;
 	}
-
+}
+#main-title-single {
+	display: none;
+	grid-row: 3 / span 6;
+	grid-column: 4 / -5;
+	position: relative;
+	@media (width < 704px) {
+		display: unset;
+	}
+	@media (width < 640px) {
+		grid-column: 4 / -5;
+	}
+	&::before {
+		content: '';
+		background-image: url('/images/arrow-v1.webp');
+		display: inline-block;
+		width: calc(var(--grid-block) * 1.1);
+		aspect-ratio: 136/214;
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+		position: absolute;
+		transform: rotate(115deg);
+		inset-inline-start: calc(var(--grid-block) * -1);
+	}
+}
+.cta-buttons {
+	display: flex;
+	flex-direction: row;
+	gap: calc(var(--grid-block) / 2);
+	& button {
+		width: calc(var(--grid-block) * 6);
+	}
+	& a {
+		width: calc(var(--grid-block) * 8);
+	}
+	@media (width < 704px) {
+		align-self: flex-end;
+		& button {
+			width: calc(var(--grid-block) * 11);
+		}
+		& a {
+			width: calc(var(--grid-block) * 11);
+		}
+	}
+	@media (width < 640px) {
+		& button {
+			width: calc(var(--grid-block) * 10);
+		}
+		& a {
+			width: calc(var(--grid-block) * 10);
+		}
+	}
+	@media (width < 600px) {
+		& button {
+			width: calc(var(--grid-block) * 9);
+		}
+		& a {
+			width: calc(var(--grid-block) * 9);
+		}
+	}
+	@media (width < 560px) {
+		& button {
+			width: calc(var(--grid-block) * 8);
+		}
+		& a {
+			width: calc(var(--grid-block) * 8);
+		}
+	}
+	@media (width < 520px) {
+		& button {
+			width: calc(var(--grid-block) * 7);
+		}
+		& a {
+			width: calc(var(--grid-block) * 7);
+		}
+	}
+	@media (width < 480px) {
+		flex-direction: column-reverse;
+		& button {
+			width: calc(var(--grid-block) * 13);
+		}
+		& a {
+			width: calc(var(--grid-block) * 13);
+		}
+	}
+	@media (width < 400px) {
+		& button {
+			width: calc(var(--grid-block) * 11);
+		}
+		& a {
+			width: calc(var(--grid-block) * 11);
+		}
+	}
 }
 </style>
