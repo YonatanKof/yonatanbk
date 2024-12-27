c7c13eea3b3c225e01566ce52f695dda9dc90059
Yonatan Kof <kof@yonatankof.com>
2024-12-27 18:12:14 +0200

    feat: Add SocialShareBtns.vue to success.vue

---
+++ components/SocialShareBtns.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { icons } from 'vue3-carousel';
+</script>
+
+<template>
+	<div class="share-box">
+		<Text tag="h4" variant="heading-x-small">Share me on</Text>
+		<div class="share-buttons">
+			<SocialShare
+				v-for="network in ['facebook', 'x', 'linkedin', 'email']"
+				:key="network"
+				:network="network"
+				:styled="true"
+				:icon="false"
+			>
+				<template #label>{{ network }}</template>
+			</SocialShare>
+		</div>
+	</div>
+</template>
+
+<style>
+.share-box{
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-xs);
+  justify-content: center;
+}
+.share-buttons {
+	display: flex;
+	flex-wrap: wrap;
+	align-items: center;
+}
+
+.social-share-button {
+	color: #fff;
+	padding: 1rem;
+	border-radius: 0;
+}
+</style>
--- pages/success.vue
+++ pages/success.vue
@@ -8,7 +8,10 @@
 					title="Grate Success"
 					description="The form has been submitted successfully, and I'll get back to you shortly. In the meanwhile, feel free to visit my site again – and make sure to share this joy with your friends, family and radome people online."
 				/>
-				<div class="zzz"><Button button-size="large" tag="a" href="/"> Go back to site</Button></div>
+				<div class="zzz">
+					<SocialShareBtns />
+					<Button button-size="large" tag="a" href="/"> Go back to site</Button>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -24,12 +27,16 @@ section {
 	box-shadow: var(--shadow-lg);
 	display: flex;
 	flex-direction: column;
+	flex-wrap: wrap-reverse;
 }
 
 .zzz {
 	position: relative;
 	z-index: 5000;
-	max-width: max-content;
+	/* max-width: 100%; */
+	display: flex;
+	justify-content: space-between;
+	gap: var(--space-m);
 }
 .torn-paper-bottom {
 	--torn-paper-image: url('/images/torn-2.webp');
