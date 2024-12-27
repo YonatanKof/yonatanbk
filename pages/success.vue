bf5cb5e6155e5dd7f3c677af9053aab95858bdd4
Yonatan Kof <kof@yonatankof.com>
2024-12-27 17:34:12 +0200

    improve: Better success page design

--- pages/success.vue
+++ pages/success.vue
@@ -1,12 +1,38 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="wrapper">
-		<LayoutContentTop
-			title="Grate success"
-			description="The form has been submitted successfully. I will get back to you as soon as possible."
-		/>
-	</div>
+	<section class="torn-paper-bottom transparent-bg">
+		<div class="wrapper">
+			<div class="box">
+				<LayoutContentTop
+					title="Grate Success"
+					description="The form has been submitted successfully, and I'll get back to you shortly. In the meanwhile, feel free to visit my site again – and make sure to share this joy with your friends, family and radome people online."
+				/>
+				<div class="zzz"><Button button-size="large" tag="a" href="/"> Go back to site</Button></div>
+			</div>
+		</div>
+	</section>
 </template>
 
-<style scoped></style>
+<style scoped>
+section {
+	height: 100vh;
+}
+.box {
+	padding: var(--space-xl);
+	background-color: var(--color-sys-invert-main);
+	box-shadow: var(--shadow-lg);
+	display: flex;
+	flex-direction: column;
+}
+
+.zzz {
+	position: relative;
+	z-index: 5000;
+	max-width: max-content;
+}
+.torn-paper-bottom {
+	--torn-paper-image: url('/images/torn-2.webp');
+	--torn-paper-bg-color: var(--color-brand-red-main);
+}
+</style>
