b30f27209d104c816eaaeac9582886baac0b365b
Yonatan Kof <kof@yonatankof.com>
2024-12-29 00:08:05 +0200

    improve: Better design in success.vue

--- pages/success.vue
+++ pages/success.vue
@@ -4,13 +4,14 @@
 	<section class="torn-paper-bottom transparent-bg">
 		<div class="wrapper">
 			<div class="box">
-				<LayoutContentTop
-					title="Grate Success"
-					description="The form has been submitted successfully, and I'll get back to you shortly. In the meanwhile, feel free to visit my site again – and make sure to share this joy with your friends, family and radome people online."
-				/>
+				<Text tag="h1" variant="secondary-title">Form Sent!</Text>
+				<Text tag="h2" variant="body-medium">I'll get back to you shortly. </Text>
+				<Text variant="body-medium"
+					>In the meanwhile, visit my site again or share with relevant friends, family or radome people online.</Text
+				>
 				<div class="zzz">
-					<SocialShareBtns />
-					<Button button-size="large" tag="a" href="/"> Go back to site</Button>
+					<SocialShareBtns :set-url="`https://www.yonatanbk.com/`" />
+					<Button button-size="medium" tag="a" href="/"> Go back to site</Button>
 				</div>
 			</div>
 		</div>
@@ -21,22 +22,57 @@
 section {
 	height: 100vh;
 }
+.wrapper {
+	max-width: 768px;
+	padding-block-start: 128px;
+	@media (width < 880px) {
+		max-width: 640px;
+	}
+	@media (width < 720px) {
+		max-width: 576px;
+	}
+	@media (width < 640px) {
+		max-width: 448px;
+	}
+	@media (width < 520px) {
+		max-width: 320px;
+	}
+	@media (width < 400px) {
+		max-width: 256px;
+	}
+}
 .box {
-	padding: var(--space-xl);
+	padding: 32px;
 	background-color: var(--color-sys-invert-main);
 	box-shadow: var(--shadow-lg);
+	min-height: 256px;
 	display: flex;
 	flex-direction: column;
 	flex-wrap: wrap-reverse;
+	gap: var(--space-s);
+	@media (width < 720px) {
+		min-height: 320px;
+	}
+	@media (width < 640px) {
+		min-height: 352px;
+	}
+	@media (width < 520px) {
+		min-height: 416px;
+	}
+	@media (width < 400px) {
+		min-height: 464px;
+	}
 }
 
 .zzz {
 	position: relative;
 	z-index: 5000;
-	/* max-width: 100%; */
 	display: flex;
 	justify-content: space-between;
 	gap: var(--space-m);
+	@media (width < 720px) {
+		flex-direction: column;
+	}
 }
 .torn-paper-bottom {
 	--torn-paper-image: url('/images/torn-2.webp');
