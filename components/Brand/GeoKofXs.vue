016e14c5244a4dd04290b1f24645753857d285cb
Yonatan Kof <kof@yonatankof.com>
2025-01-01 11:33:56 +0200

    improve: Better social assets

--- components/Brand/GeoKofXs.vue
+++ components/Brand/GeoKofXs.vue
@@ -7,24 +7,25 @@
 		xmlns="http://www.w3.org/2000/svg"
 		xmlns:xlink="http://www.w3.org/1999/xlink"
 	>
-		<g id="🦍-Kof" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-			<g id="orange" transform="translate(0, 13)" fill="var(--color-brand-orange-main)">
-				<rect x="28" y="0" width="4" height="15"></rect>
-				<rect x="18" y="15" width="10" height="4"></rect>
-				<rect x="14" y="6" width="4" height="9"></rect>
-				<rect x="4" y="15" width="10" height="4"></rect>
-				<rect x="0" y="0" width="4" height="15"></rect>
+		<g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+			<g id="red" transform="translate(2, 12)" fill="var(--color-brand-red-main)">
+				<rect id="Rectangle-Copy" x="24" y="2" width="4" height="12"></rect>
+				<rect id="Rectangle-Copy-3" x="12" y="8" width="4" height="6"></rect>
+				<rect id="Rectangle-Copy-4" x="4" y="14" width="8" height="4"></rect>
+				<rect id="Rectangle-Copy-6" x="16" y="14" width="8" height="4"></rect>
+				<rect id="Rectangle-Copy-5" x="0" y="0" width="4" height="14"></rect>
 			</g>
-			<g id="blue" transform="translate(0, 0)" fill="var(--color-brand-blue-main)">
-				<rect x="0" y="0" width="11" height="4"></rect>
-				<rect x="0" y="0" width="4" height="10"></rect>
-				<rect x="7" y="0" width="4" height="25"></rect>
+			<g id="blue" transform="translate(2, 2)" fill="var(--color-brand-blue-main)">
+				<rect id="Rectangle" x="0" y="0" width="10" height="4"></rect>
+				<rect id="Rectangle" x="0" y="0" width="4" height="8"></rect>
+				<rect id="Rectangle" x="6" y="0" width="4" height="22"></rect>
 			</g>
-			<rect id="green" fill="var(--color-brand-green-main)" x="14" y="9.09494702e-13" width="4" height="16"></rect>
-			<g id="red" transform="translate(21, 0)" fill="var(--color-brand-red-main)">
-				<rect x="0" y="0" width="11" height="4"></rect>
-				<rect x="7" y="0" width="4" height="10"></rect>
-				<rect x="0" y="7" width="4" height="18"></rect>
+			<rect id="green" fill="var(--color-brand-green-main)" x="14" y="2" width="4" height="16"></rect>
+			<g id="orange" transform="translate(20, 2)" fill="var(--color-brand-orange-main)">
+				<rect id="Rectangle" x="0" y="0" width="10" height="4"></rect>
+				<rect id="Rectangle-Copy-2" x="0" y="6" width="10" height="4"></rect>
+				<rect id="Rectangle" x="6" y="0" width="4" height="8"></rect>
+				<rect id="Rectangle" x="0" y="6" width="4" height="16"></rect>
 			</g>
 		</g>
 	</svg>
