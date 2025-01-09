79c3251d50aa369c019ebf6faccf9707cc33c64a
Yonatan Kof <kof@yonatankof.com>
2025-01-09 21:22:51 +0200

    improve: Add second content to ContentTop.vue

--- components/Layout/ContentTop.vue
+++ components/Layout/ContentTop.vue
@@ -4,16 +4,23 @@ defineProps({
 		type: Boolean,
 		default: true,
 	},
+	isDescIi: {
+		type: Boolean,
+		default: false,
+	},
 });
 </script>
 
 <template>
 	<div>
-		<Text tag="h1" variant="main-title">
+		<Text tag="h1" variant="main-title" text-style="text-balance">
 			<slot name="title">Proj name!</slot>
 		</Text>
-		<Text variant="large-text" class="this-text" v-if="isDesc">
+		<Text variant="large-text" class="max-ch" v-if="isDesc">
 			<slot name="desc"></slot>
+		</Text>
+		<Text variant="large-text" class="max-ch" v-if="isDescIi">
+			<slot name="desc-ii"></slot>
 		</Text>
 	</div>
 </template>
@@ -33,7 +40,7 @@ div {
 		padding-block-end: var(--space-l);
 	}
 }
-.this-text {
+max-ch {
 	max-width: 55ch;
 }
 </style>
