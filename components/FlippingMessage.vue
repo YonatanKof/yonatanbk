408192f377c9d6274fd01bfe4e14a412ed7c1e08
Yonatan Kof <kof@yonatankof.com>
2025-05-17 20:38:55 +0300

    improve: Cleanups and text tweaks in utils.css, FlippingMessage.vue & Hello.vue

--- components/FlippingMessage.vue
+++ components/FlippingMessage.vue
@@ -5,12 +5,11 @@ import type { TypedOptions } from '@duskmoon/vue3-typed-js';
 const options: TypedOptions = {
 	strings: [
 		'Design Development Delivery',
-		// '<span class="this-text-on-big">From </span> <span class="text-bold color-brand-red">Design </span> to <span class="text-bold color-brand-blue">Production</span>',
-		'</span> <span class="text-bold color-brand-red">Designing </span> for <span class="text-bold color-brand-blue">Production</span>',
+		'<span class="this-text-on-big">From </span> <span class="text-bold color-brand-red">Design </span> to <span class="text-bold color-brand-blue">Production</span>',
+		// '</span> <span class="text-bold color-brand-red">Designing </span> for <span class="text-bold color-brand-blue">Production</span>',s
 		'<span class="text-bold color-brand-green">Building</span> design <span class="text-bold color-brand-orange">Systems</span>',
 		'<span class="text-bold color-brand-orange">Steering</span> <span class="this-text-on-big">cross</span> <span class="this-text-on-small">x</span>-functional <span class="text-bold color-brand-blue">Teams</span>',
 		'<span class="text-bold color-brand-blue">Leading</span> design <span class="text-bold color-brand-red">Initiatives</span>',
-		// '<span class="text-bold color-brand-red">Managing</span> design <span class="text-bold color-brand-blue">Backlogs</span>',
 		'<span class="text-bold color-brand-orange">Executing</span> design <span class="text-bold color-brand-green">Research</span>',
 	],
 	loop: true,
@@ -29,9 +28,6 @@ const options: TypedOptions = {
 					<Text class="typing" text-style="text-end color-slight" tag="h2" variant="compressed-title"> </Text>
 				</Typed>
 			</span>
-			<Text class="hide" text-style="text-end color-slight" tag="h2" variant="compressed-title">
-				in startups and orgs
-			</Text>
 		</div>
 	</div>
 </template>
@@ -46,12 +42,6 @@ span,
 	flex-direction: row;
 	gap: var(--space-2xs);
 	justify-content: end;
-	/* @media (width < 720px) {
-		flex-direction: column;
-	} */
-	/* @media (width < 480px) {
-		flex-flow: row;
-	} */
 }
 h2 {
 	@media (width < 1152px) {
@@ -78,4 +68,5 @@ h2 {
 	@media (width < 560px) {
 		display: unset;
 	}
-}</style>
+}
+</style>
--- components/Section/Hello.vue
+++ components/Section/Hello.vue
@@ -21,27 +21,13 @@ const handleConfirm = () => {
 						>Yonatan <span class="no-brake">Ben Knaan</span>
 					</Text>
 				</div>
-				<!-- <Text class="text-on-big" id="main-title-1" tag="h1" variant="main-title" text-style="grid-size-big"> -->
-				<Text id="main-title-1" tag="h1" variant="main-title" text-style="grid-size-big">
-					<!-- a full stack designer -->
-					a full stack
-				</Text>
-				<!-- <Text class="text-on-small" id="main-title-1" tag="h1" variant="main-title" text-style="grid-size-big">
-					a full stack
-				</Text> -->
+				<Text id="main-title-1" tag="h1" variant="main-title" text-style="grid-size-big"> a full stack </Text>
 			</div>
 
 			<div id="main-messages-bottom">
-				<!-- <Text class="text-on-big" id="main-title-2" text-style="grid-size-big text-end" tag="h1" variant="main-title"> -->
 				<Text id="main-title-2" text-style="grid-size-big text-end" tag="h1" variant="main-title">
-					<!-- who leads <span class="no-brake">design initiatives</span> -->
-					<!-- leading initiatives -->
 					product designer
-					<!-- design lead -->
 				</Text>
-				<!-- <Text class="text-on-small" id="main-title-2" text-style="grid-size-big text-end" tag="h1" variant="main-title">
-					design lead
-				</Text> -->
 				<FlippingMessage id="flipping-message" />
 			</div>
 			<span id="logos-cta">
@@ -113,41 +99,28 @@ const handleConfirm = () => {
 	grid-template-rows: repeat(var(--grid-num-height), var(--grid-block));
 	grid-template-columns: repeat(var(--grid-num), var(--grid-block));
 	justify-content: center;
-	/* @media (width < 1216px) {
-		--grid-num: 37;
-	} */
-	/* @media (width < 1152px) {
-		--grid-num: 35;
-		grid-template-rows: repeat(24, var(--grid-block));
-	} */
+	@media (width < 1152px) {
+		--grid-num: 31;
+	}
 	/* @supports (background: -webkit-named-image(i)) {
 		@media (width < 1152px) {
 			--grid-num: 37;
 			grid-template-rows: repeat(23, var(--grid-block));
 		}
 	} */
-	/* @media (width < 976px) {
-		--grid-num: 33;
-	} */
-	@media (width < 896px) {
-		--grid-num: 31;
+	@media (width < 1024px) {
+		--grid-num: 29;
+	}
+	@media (width < 880px) {
+		--grid-num: 25;
 	}
 	@media (width < 720px) {
 		--grid-num: 23;
 		--grid-num-height: 26;
 	}
-	/* @media (width < 640px) {
-		--grid-num: 21;
-	} */
 	@media (width < 620px) {
 		--grid-num: 21;
 	}
-	/* @media (width < 560px) {
-		--grid-num: 17;
-	} */
-	/* @media (width < 520px) {
-		--grid-num: 19;
-	} */
 	@media (width < 560px) {
 		--grid-num: 19;
 		--grid-num-height: 36;
@@ -158,9 +131,6 @@ const handleConfirm = () => {
 	@media (width < 380px) {
 		--grid-num: 15;
 	}
-	/* @media (width < 340px) {
-		--grid-num: 15;
-	} */
 }
 
 #main-messages-top {
@@ -169,9 +139,9 @@ const handleConfirm = () => {
 	display: grid;
 	grid-template-rows: subgrid;
 	grid-template-columns: subgrid;
-	/* @media (width < 1216px) {
-		grid-column: 3 / -1;
-		} */
+	@media (width < 1152px) {
+		grid-column: 2 / -1;
+	}
 	@media (width < 896px) {
 		grid-column: 3 / -1;
 	}
@@ -194,25 +164,16 @@ const handleConfirm = () => {
 			grid-row: 6 / span 5;
 		}
 	} */
-	/* @media (width < 1152px) {
-		grid-column: 1 / -1;
-		} */
-	/* @supports (background: -webkit-named-image(i)) {
-		@media (width < 976px) {
-			grid-row: 6 / span 9;
-		}
-	} */
-	@media (width < 896px) {
-		grid-column: 1 / -4;
+	@media (width < 1024px) {
+		grid-column: 1 / -2;
+	}
+	@media (width < 880px) {
+		grid-column: 1 / -2;
 	}
 	@media (width < 720px) {
 		grid-row: 7 / span 6;
 		grid-column: 1 / -1;
-		/* margin-inline: calc(var(--grid-block) * -1); */
 	}
-	/* @media (width < 520px) {
-		grid-row: 6 / span 9;
-	} */
 	@media (width < 560px) {
 		grid-row: 10 / span 10;
 	}
@@ -231,21 +192,10 @@ const handleConfirm = () => {
 	@media (width < 720px) {
 		grid-column: 1 / -1;
 	}
-	/* @media (width < 640px) {
-		grid-column: 2 / -1;
-	} */
-	/* @media (width < 600px) {
-		grid-column: 1 / -1;
-		} */
 	@media (width < 560px) {
 		gap: var(--space-xs);
-		/* margin-inline-start: calc(var(--grid-block) * -1); */
 		grid-column: 2 / -1;
 	}
-	/* @media (width < 520px) {
-		grid-column: 1 / -1;
-		margin-inline-start: calc(var(--grid-block) * -2);
-	} */
 	@media (width < 560px) {
 		grid-column: 1 / -1;
 	}
@@ -257,18 +207,6 @@ const handleConfirm = () => {
 	grid-row: 3 / span 3;
 	grid-column: 3 / -6;
 	position: relative;
-	/* @media (width < 1216px) {
-		grid-column: 4 / -5;
-	} */
-	/* @media (width < 1152px) {
-		grid-column: 3 / -4;
-	} */
-	/* @media (width < 976px) {
-		grid-column: 3 / -3;
-	} */
-	/* @media (width < 896px) {
-		grid-column: 3 / -2;
-	} */
 	@media (width < 792px) {
 		grid-column: 3 / -2;
 	}
@@ -276,18 +214,6 @@ const handleConfirm = () => {
 		grid-row: 3 / span 6;
 		grid-column: 4 / -1;
 	}
-	/* @media (width < 640px) {
-		grid-column: 3 / -1;
-	} */
-	/* @media (width < 600px) {
-		grid-column: 3 / -3;
-	} */
-	/* @media (width < 560px) {
-		grid-column: 2 / -2;
-	} */
-	/* @media (width < 520px) {
-		grid-column: 1 / -1;
-	} */
 	@media (width < 560px) {
 		grid-row: 3 / span 6;
 		grid-column: 3 / -6;
@@ -313,24 +239,14 @@ const handleConfirm = () => {
 	grid-row: 1 / span 3;
 	position: relative;
 	grid-column: 1 / -1;
-
-	/* @media (width < 1344px) {
-		grid-column: 1 / -1;
-	} */
-	/* @media (width < 1152px) {
-		grid-row: 1 / -1;
-	} */
 	@media (width < 720px) {
 		grid-row: 1 / span 3;
 		text-align: center;
+		margin-inline: calc(var(--grid-block) * -1);
+	}
+	@media (width < 620px) {
 		margin-inline: calc(var(--grid-block) * -2);
 	}
-	/* @media (width < 620px) {
-		grid-column: 2 / -1;
-		} */
-	/* @media (width < 520px) {
-			font-size: calc(var(--grid-block) * 2.9);
-			} */
 	@media (width < 560px) {
 		text-align: start;
 		grid-row: 1 / span 6;
@@ -349,16 +265,12 @@ const handleConfirm = () => {
 	@media (width < 1344px) {
 		grid-column: 1 / -1;
 	}
-	/* @media (width < 1152px) {
-		grid-row: 7 / span 2;
-	} */
 	/* @supports (background: -webkit-named-image(i)) {
 		@media (width < 976px) {
 			grid-row: 7 / span 3;
 		}
 	} */
 	@media (width < 720px) {
-		/* grid-row: 5 / span 3; */
 		grid-column: 1 / -1;
 	}
 	@media (width < 620px) {
@@ -381,17 +293,11 @@ const handleConfirm = () => {
 	justify-content: space-between;
 	grid-column: 1 / -1;
 	@media (width < 1152px) {
-		/* grid-row: 16 / span 3; */
-		grid-column: 3 / -3;
+		grid-column: 1 / -1;
 	}
 	/* @supports (background: -webkit-named-image(i)) {
 		@media (width < 1152px) {
 			grid-row: 13 / span 3;
-		}
-	} */
-	/* @supports (background: -webkit-named-image(i)) {
-		@media (width < 976px) {
-			grid-row: 16 / span 3;
 		}
 	} */
 	@media (width < 720px) {
@@ -399,9 +305,6 @@ const handleConfirm = () => {
 		grid-row: 14 / span 7;
 		flex-direction: column-reverse;
 	}
-	/* @media (width < 560px) {
-		grid-column: 1 / -1;
-	} */
 	@media (width < 560px) {
 		grid-column: 3 / -3;
 		grid-row: 20 / span 12;
@@ -414,17 +317,11 @@ const handleConfirm = () => {
 	max-width: calc(var(--grid-block) * 16);
 	@media (width < 1408px) {
 	}
-	/* @media (width < 1216px) {
-		max-width: calc(var(--grid-block) * 18);
-	} */
 	@media (width < 1152px) {
 		max-width: calc(var(--grid-block) * 13);
 	}
-	/* @media (width < 976px) {
-		max-width: calc(var(--grid-block) * 13);
-	} */
-	@media (width < 896px) {
-		max-width: calc(var(--grid-block) * 11);
+	@media (width < 880px) {
+		max-width: calc(var(--grid-block) * 9);
 	}
 	@media (width < 720px) {
 		max-width: 100%;
@@ -444,14 +341,6 @@ const handleConfirm = () => {
 		width: calc(var(--grid-block) * 6);
 		padding-inline: unset;
 	}
-	/* @media (width < 1152px) {
-		& .btn-form {
-			width: calc(var(--grid-block) * 7);
-		}
-		& a {
-			width: calc(var(--grid-block) * 4);
-		}
-	} */
 	@media (width < 720px) {
 		align-self: flex-end;
 		& .btn-form {
@@ -463,14 +352,6 @@ const handleConfirm = () => {
 			width: calc(var(--grid-block) * 9);
 		}
 	}
-	/* @media (width < 640px) {
-		& .btn-form {
-			width: calc(var(--grid-block) * 8);
-		}
-		& .btn-linkedin {
-			width: calc(var(--grid-block) * 8);
-		}
-	} */
 	@media (width < 620px) {
 		& .btn-form {
 			width: calc(var(--grid-block) * 8);
@@ -479,22 +360,6 @@ const handleConfirm = () => {
 			width: calc(var(--grid-block) * 8);
 		}
 	}
-	/* @media (width < 560px) {
-		& .btn-form {
-			width: calc(var(--grid-block) * 10);
-		}
-		& .btn-linkedin {
-			width: calc(var(--grid-block) * 10);
-		}
-	} */
-	/* @media (width < 520px) {
-		& .btn-form {
-			width: calc(var(--grid-block) * 8);
-		}
-		& .btn-linkedin {
-			width: calc(var(--grid-block) * 8);
-		}
-	} */
 	@media (width < 560px) {
 		flex-direction: column-reverse;
 		gap: calc(var(--grid-block) / 2);
@@ -505,14 +370,6 @@ const handleConfirm = () => {
 			width: calc(var(--grid-block) * 15);
 		}
 	}
-	/* @media (width < 440px) {
-		& .btn-form {
-			width: calc(var(--grid-block) * 14);
-		}
-		& .btn-linkedin {
-			width: calc(var(--grid-block) * 14);
-		}
-	} */
 	@media (width < 380px) {
 		& .btn-form {
 			width: calc(var(--grid-block) * 13);
